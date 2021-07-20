2e901256a59379fada600bea085adc1810a0540e
Arnicha <arnichamichai@gmail.com>
2021-07-20 23:07:16 +0700

    card carbon by Aon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import '../styles/scss/style.scss'
+import Card from './../components/card-carbon/Card'
 
 export default function Home() {
   return (
     <div id="indexPage">
       <div className="home">
+        <Card/>
         <label id="welcomeText">Welcome To FindMyMate</label>
       </div>
     </div>
