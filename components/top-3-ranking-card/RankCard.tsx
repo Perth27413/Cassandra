322d46765b10abda90673e35e43315d1f770ebee
udomsak <specialclub13@gmail.com>
2021-07-21 18:18:41 +0700

    top rank by Udome

--- components/top-3-ranking-card/RankCard.tsx
+++ components/top-3-ranking-card/RankCard.tsx
@@ -2,42 +2,51 @@ import React from 'react'
 import './RankCard.scss'
 
 export default function Card(): JSX.Element {
-    const [cardList, setCardList] = React.useState([
-        {
-            topic: 'Most Carbon Used',
-            topList: ['perth', 'bio', 'jill']
-        },
-        {
-            topic: 'Least Carbon Used',
-            topList: ['uDome', 'uGo', 'bung']
-        },
-    ])
+const [cardList, setCardList] = React.useState([
+  {
+      topic: 'Most Carbon Used',
+      topList: ['perth', 'bio', 'jill']
+  },
+  {
+      topic: 'Least Carbon Used',
+      topList: ['uDome', 'uGo', 'bung']
+  },
+])
 
 return (
-        <div id='rankCard'>
-            {cardList.map((item, index) => {
-                return (
-                    <div key={index} className='rank-card-item'>
-                        <div className='rank-top'>
-                            <div className='rank-topic'>
-                                <label className='text-topic'>{item.topic}</label>
-                            </div>
-                            <div className="list-info">
-                                <div className="profile-part">
-                                    <div className="name-box">
-                                        <div className="rider-name">{item.topList}</div>
-                                        <div className="occupation">{item.topList}</div>
-                                    </div>
-                                </div>
-                                <div className="carbon-part">
-                                    <div className="carbon-amount">{item.topList}</div>
-                                </div>
+  <div id='rankCard'>
+      {cardList.map((item, index) => {
+          return (
+              <div key={index} className='rank-card-item'>
+                  <div className='rank-top'>
+                      <div className='rank-topic'>
+                          <label className='text-topic'>{item.topic}</label>
+                      </div>
+                  </div>
+                  { item.topList.map((value: string, num: number) => {
+                    return (
+                       <div key={value} className="list-info">
+                        <div className="profile">
+                            <img className="picture-box" src="https://pyxis.nymag.com/v1/imgs/3d4/0aa/89125115b0e10b94e3378d484712450727-25-thanos.rhorizontal.w700.jpg" alt="" />
+                            <div className="text-box">
+                                <div className="name-box">{value} {value}</div>
+                                <div className="position-box">{value}</div>
                             </div>
                         </div>
-                    </div>
-                )
-            })}
+                        <div className="carbon">
+                          <div className={`carbon-box
+                            ${index === 0 ? num === 1 ? 'orange' : num === 2 ? 'yellow' : '' : num === 0 ? 'darkgreen' : num === 1 ? 'green' : 'lightgreen'}
+                          `}>
+                          </div>
+                        </div>
+                      </div>
+                    )
+                  })
+                  }
+              </div>
+          )
+      })}
 
-        </div>
-    )
+  </div>
+)
 }
