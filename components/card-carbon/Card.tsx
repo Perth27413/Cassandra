8bf3fe923bc5e96c7af99fd4423593a2993c6d00
Arnicha <arnichamichai@gmail.com>
2021-07-21 13:17:30 +0700

    card carbon v2 , active

--- components/card-carbon/Card.tsx
+++ components/card-carbon/Card.tsx
@@ -1,47 +1,65 @@
 import React from 'react'
 import './Card.scss'
 
-interface IProps {}
+export default function Card(): JSX.Element {
+  const [number, setNumber] = React.useState(0)
+  const [cardList, setCardList] = React.useState([
+    {
+      topic: 'Today Carbon',
+      persent: '+3%',
+      volume: '6323',
+      gram: 'gram'
+    },
+    {
+      topic: 'Weekly Carbon',
+      persent: '+3%',
+      volume: '6323',
+      gram: 'gram'
+    },
+    {
+      topic: 'Average Carbon/Distance',
+      persent: '+3%',
+      volume: '59',
+      gram: 'gram'
+    },
+  ])
 
-interface IState {
-}
-
-class Card extends React.PureComponent<IProps, IState> {
-  constructor(props: IProps) {
-    super(props)
+  const active = (numCurrent: number) => {
+    if (numCurrent == 0) {
+      setNumber(0)
+    } else if (numCurrent == 1) {
+      setNumber(1)
+    } else if (numCurrent == 2) {
+      setNumber(2)
+    } else {
+      setNumber(0)
+    }
   }
-  render(): JSX.Element {
-    return (
-      <div id='card'>
-        <div className='card-item'>
-          <div>
-            <div>
-              <label>Today Carbon</label>
-            </div>
-            <div>
-              <label>+3%</label>
-            </div>
-          </div>
-          <div>
-            <p >62132</p>
-          </div>
-          <div>
-            <label>gram</label>
-          </div>
-        </div>
-        <div className='card-item'>
-          <p>Weekly Carbon</p>
-          <p>62132</p>
-          <p >gram</p>
-        </div>
-        <div className='card-item'>
-          <p>Average carbon/km</p>
-          <p>62132</p>
-          <p>gram</p>
-        </div>
-      </div>
-    )
-  }
-}
 
-export default Card
+  return (
+    <div id='card'>
+
+      {cardList.map((item, index) => {
+        return (
+          <div key={index} className={`card-item ${index == number ? 'active' : ''}`} onClick={()=>active(index)}>
+            <div className='top'>
+              <div className='con-topic'>
+                <label className='text-topic'>{item.topic}</label>
+              </div>
+              <div className='con-percent'>
+                <label className='percent'>{item.persent}</label>
+              </div>
+            </div>
+            <div className='mid'>
+              <p className='volume'>{item.volume}</p>
+            </div>
+            <div className='bottom'>
+              <label className='text-gram'>{item.gram}</label>
+            </div>
+          </div>
+        )
+      })}
+
+    </div>
+  )
+}
